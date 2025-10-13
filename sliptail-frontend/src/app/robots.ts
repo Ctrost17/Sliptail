8553e52fb20a3c2fb34ca4d0253b68da638b762d
Charles Trost <ctrost@sliptail.com>
2025-10-12 21:58:54 -0500

    Fix Site map

--- sliptail-frontend/src/app/robots.ts
+++ sliptail-frontend/src/app/robots.ts
@@ -3,7 +3,7 @@ import type { MetadataRoute } from "next";
 
 export default function robots(): MetadataRoute.Robots {
   return {
-    rules: [{ userAgent: "*", allow: "/" , disallow: ["/creators/"]}],
+    rules: [{ userAgent: "*", allow: "/" , disallow: ["/creators/","/admin","/dashboard","/settings","/terms","/privacy","/notifications,/purchases"]}],
     sitemap: "https://sliptail.com/sitemap.xml",
   };
 }
